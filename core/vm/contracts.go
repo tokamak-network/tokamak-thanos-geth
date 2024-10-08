a4230ae40171cad0321ddc314bcbd23c094d9256
rlgns98kr <rlgns98kr@gmail.com>
2024-10-08 18:43:32 +0900

    [OR-1881] add max input size of bn256Pairing

--- core/vm/contracts.go
+++ core/vm/contracts.go
@@ -118,7 +118,7 @@ var PrecompiledContractsFjord = map[common.Address]PrecompiledContract{
 	common.BytesToAddress([]byte{5}):          &bigModExp{eip2565: true},
 	common.BytesToAddress([]byte{6}):          &bn256AddIstanbul{},
 	common.BytesToAddress([]byte{7}):          &bn256ScalarMulIstanbul{},
-	common.BytesToAddress([]byte{8}):          &bn256PairingIstanbul{},
+	common.BytesToAddress([]byte{8}):          &bn256PairingFjord{},
 	common.BytesToAddress([]byte{9}):          &blake2F{},
 	common.BytesToAddress([]byte{0x0a}):       &kzgPointEvaluation{},
 	common.BytesToAddress([]byte{0x01, 0x00}): &p256Verify{},
@@ -549,6 +549,9 @@ var (
 
 	// errBadPairingInput is returned if the bn256 pairing input is invalid.
 	errBadPairingInput = errors.New("bad elliptic curve pairing size")
+
+	// errBadPairingInputSize is returned if the bn256 pairing input size is invalid.
+	errBadPairingInputSize = errors.New("bad elliptic curve pairing input size")
 )
 
 // runBn256Pairing implements the Bn256Pairing precompile, referenced by both
@@ -580,6 +583,21 @@ func runBn256Pairing(input []byte) ([]byte, error) {
 		return true32Byte, nil
 	}
 	return false32Byte, nil
+}
+
+// bn256PairingGranite implements a pairing pre-compile for the bn256 curve
+// conforming to Granite consensus rules.
+type bn256PairingFjord struct{}
+
+// RequiredGas returns the gas required to execute the pre-compiled contract.
+func (c *bn256PairingFjord) RequiredGas(input []byte) uint64 {
+	return new(bn256PairingIstanbul).RequiredGas(input)
+}
+func (c *bn256PairingFjord) Run(input []byte) ([]byte, error) {
+	if len(input) > int(params.Bn256PairingMaxInputSizeFjord) {
+		return nil, errBadPairingInputSize
+	}
+	return runBn256Pairing(input)
 }
 
 // bn256PairingIstanbul implements a pairing pre-compile for the bn256 curve
